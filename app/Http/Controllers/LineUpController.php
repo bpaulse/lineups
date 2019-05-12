dd0910f372c54613dc6289a3c82238912e5bddf7
Bevan Paulse <bevan@bwsonline.co.za>
2019-05-12 22:50:15 +0200

    Commit #3

--- app/Http/Controllers/LineUpController.php
+++ app/Http/Controllers/LineUpController.php
@@ -168,3 +168,85 @@ class LineUpController extends Controller
         //
     }
 }
+
+
+702
+12 May 2019
+
+'Weekend Breakfast with Phemelo Motene', '06:00','10:00'
+'Soulful Sundays with Kenny Maistry', '10:00','13:00'
+'Soulful Sundays with Paul Mtirara', '13:00','16:00'
+'Soulful Sundays with Nonn Botha', '16:00','19:00'
+'SportsTalk with Buhle Madulini', '19:00','21:00'
+'NightTalk with Gushwell Brooks', '21:00','00:00'
+
+
+
+INSERT INTO `show`(`timeslot_id`, `show_name`) VALUES (14,'Weekend Breakfast with Phemelo Motene');
+INSERT INTO `show`(`timeslot_id`, `show_name`) VALUES (14,'Soulful Sundays with Kenny Maistry');
+INSERT INTO `show`(`timeslot_id`, `show_name`) VALUES (14,'Soulful Sundays with Paul Mtirara');
+INSERT INTO `show`(`timeslot_id`, `show_name`) VALUES (14,'Soulful Sundays with Nonn Botha');
+INSERT INTO `show`(`timeslot_id`, `show_name`) VALUES (14,'SportsTalk with Buhle Madulini');
+INSERT INTO `show`(`timeslot_id`, `show_name`) VALUES (14,'NightTalk with Gushwell Brooks');
+
+
+
+
+
+INSERT INTO `users` (`username`, `firstname`, `lastname`) VALUES ('pmotene','Phemelo','Motene');
+INSERT INTO `users` (`username`, `firstname`, `lastname`) VALUES ('kmaistry','Kenny','Maistry');
+INSERT INTO `users` (`username`, `firstname`, `lastname`) VALUES ('pmtirara','Paul','Mtirara');
+INSERT INTO `users` (`username`, `firstname`, `lastname`) VALUES ('nbotha','Nonn','Botha');
+INSERT INTO `users` (`username`, `firstname`, `lastname`) VALUES ('bmadulini','Buhle','Madulini');
+INSERT INTO `users` (`username`, `firstname`, `lastname`) VALUES ('gbrooks','Gushwell','Brooks');
+
+
+WEEKDAYS
+2019-05-13
+'05:00','06:00' 'Alex Caige'
+'06:00','09:00' '947 Breakfast Club with Anele'
+'09:00','10:00' '947 Beats @ Work'
+'10:00','15:00' 'Ayanda MVP'
+'15:00','16:00' '947 Beats @ Work'
+'16:00','19:00' 'Afternoon Drive with Greg & Lucky', 'galdridge''Greg Aldridge' 'luckydup', 'Lucky Du Plessis'
+'19:00','22:00' 'The Night Show with Zweli & Mantsoe' 'zmbhele','Zweli Mbhele','mpout','Mantsoe Pout'
+'22:00','01:00' 'MAC G'
+'19:00','22:00' '(Friday) 947 Bloc Party'
+SATURDAY
+'06:00','10:00' 'Weekend Breakfast with Rob Vember'
+'10:00','14:00' 'Coca Cola Top 40 SA with Zweli and Mantsoe'
+'14:00','18:00' 'Ayanda MVP'
+'18:00','21:00' '947 Bloc Party'
+'21:00','01:00' '947 Bloc Party with MAC G'
+SUNDAY
+'06:00','10:00' 'Weekend Breakfast with Rob Vember'
+'10:00','18:00' '947 Love Sundays'
+'18:00','20:00' 'eUnite with Euphonik'
+'20:00','22:00' 'Karabo Ntshweng'
+
+
+
+
+Weekend Breakfast with Africa
+06:00 — 10:00
+This is the go-to place to know about the latest fashion, food, trends and everything that is happening in Cape Town.
+
+Solid Gold Sunday
+10:00 — 14:00
+Saturdays are Solid Gold with a great selection of your favourite golden oldies.
+
+Solid Gold Sunday
+14:00 — 17:00
+Saturdays are Solid Gold with a great selection of your favourite golden oldies.
+
+Jazz Sessions
+17:00 — 19:00
+Two hours of local and international Jazz every Sunday with Gary van Dyk.
+
+SportsTalk with Buhle Madulini
+19:00 — 21:00
+The best bits from the world of sport with comment and conversation that is on point and off the cuff, presented by Buhle Madulini.
+
+Tonight with Lester Kiewit
+21:00 — 23:00
+Get the best of both worlds with Lester Kiewit, a writer for the Mail & Guardian by day and presenter by night. Rather than looking back at the news of the day, explore new subjects and new perspectives.
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # lineups
+
+Please make sure to run the queries in queries.txt
+
+List of End Points:
+
+1. 
